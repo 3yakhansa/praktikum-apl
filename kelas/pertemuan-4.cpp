3451965672d6f80a42d412f457e648c28323a927
3yakhansa <3yanisa@gmail.com>
2025-04-11 10:01:09 +0800

    Finish Post Test 4

---
+++ kelas/pertemuan-4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// int tambah(int a,int b);
+
+// int main()
+// {
+//     int hasil = tambah(5,6);
+//     cout << hasil;
+//     return 0;
+// }
+
+// void perulangan();
+// {   
+//     for (int i = 0; i<5; i++)
+//     {
+//         if (i ==2)
+//         {
+//             return;
+//         }cout << i << endl
+//     }
+// }
+
+// int faktorial(int n){
+//     if (n==1) 
+//         return 1;
+//     return n*faktorial(n-1);
+// }
+
+// int main()
+// {
+//     cout<<faktorial(5) <<endl;
+    
+// }
+
